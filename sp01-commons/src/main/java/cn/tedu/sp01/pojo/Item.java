73cf317e27d0f09def6c9e1b2ceb354a615bc21c
Administrator <Administrator@DESKTOP-AMDNKA95O>
2020-08-01 12:13:59 +0800

    test

---
+++ sp01-commons/src/main/java/cn/tedu/sp01/pojo/Item.java
@@ -0,0 +1,15 @@
+package cn.tedu.sp01.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**这个类中包含商品的基本信息*/
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+	private Integer id;      //商品的Id
+	private String name;	 //商品的名字
+	private Integer number;  //商品的库存
+}
